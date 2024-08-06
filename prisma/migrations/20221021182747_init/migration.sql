7863abaa9eb9b0de4c935b32f6ac197ba479ac66
jinghanx <jinghanx@gmail.com>
2024-08-06 16:50:28 -0700

    change to postgres migration and add vercel-build

--- prisma/migrations/20221021182747_init/migration.sql
+++ prisma/migrations/20221021182747_init/migration.sql
@@ -1,77 +1,77 @@
 -- CreateTable
-CREATE TABLE `accounts` (
-    `id` VARCHAR(191) NOT NULL,
-    `userId` VARCHAR(191) NOT NULL,
-    `type` VARCHAR(191) NOT NULL,
-    `provider` VARCHAR(191) NOT NULL,
-    `providerAccountId` VARCHAR(191) NOT NULL,
-    `refresh_token` TEXT NULL,
-    `access_token` TEXT NULL,
-    `expires_at` INTEGER NULL,
-    `token_type` VARCHAR(191) NULL,
-    `scope` VARCHAR(191) NULL,
-    `id_token` TEXT NULL,
-    `session_state` VARCHAR(191) NULL,
-    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+CREATE TABLE "accounts" (
+    "id" VARCHAR(191) NOT NULL,
+    "userId" VARCHAR(191) NOT NULL,
+    "type" VARCHAR(191) NOT NULL,
+    "provider" VARCHAR(191) NOT NULL,
+    "providerAccountId" VARCHAR(191) NOT NULL,
+    "refresh_token" TEXT,
+    "access_token" TEXT,
+    "expires_at" INTEGER,
+    "token_type" VARCHAR(191),
+    "scope" VARCHAR(191),
+    "id_token" TEXT,
+    "session_state" VARCHAR(191),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    UNIQUE INDEX `accounts_provider_providerAccountId_key`(`provider`, `providerAccountId`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
-CREATE TABLE `sessions` (
-    `id` VARCHAR(191) NOT NULL,
-    `sessionToken` VARCHAR(191) NOT NULL,
-    `userId` VARCHAR(191) NOT NULL,
-    `expires` DATETIME(3) NOT NULL,
-
-    UNIQUE INDEX `sessions_sessionToken_key`(`sessionToken`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    UNIQUE ("provider", "providerAccountId"),
+    PRIMARY KEY ("id")
+);
 
 -- CreateTable
-CREATE TABLE `users` (
-    `id` VARCHAR(191) NOT NULL,
-    `name` VARCHAR(191) NULL,
-    `email` VARCHAR(191) NULL,
-    `emailVerified` DATETIME(3) NULL,
-    `image` VARCHAR(191) NULL,
-    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+CREATE TABLE "sessions" (
+    "id" VARCHAR(191) NOT NULL,
+    "sessionToken" VARCHAR(191) NOT NULL,
+    "userId" VARCHAR(191) NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
 
-    UNIQUE INDEX `users_email_key`(`email`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    UNIQUE ("sessionToken"),
+    PRIMARY KEY ("id")
+);
 
 -- CreateTable
-CREATE TABLE `verification_tokens` (
-    `identifier` VARCHAR(191) NOT NULL,
-    `token` VARCHAR(191) NOT NULL,
-    `expires` DATETIME(3) NOT NULL,
+CREATE TABLE "users" (
+    "id" VARCHAR(191) NOT NULL,
+    "name" VARCHAR(191),
+    "email" VARCHAR(191),
+    "emailVerified" TIMESTAMP(3),
+    "image" VARCHAR(191),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    UNIQUE INDEX `verification_tokens_token_key`(`token`),
-    UNIQUE INDEX `verification_tokens_identifier_token_key`(`identifier`, `token`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    UNIQUE ("email"),
+    PRIMARY KEY ("id")
+);
 
 -- CreateTable
-CREATE TABLE `posts` (
-    `id` VARCHAR(191) NOT NULL,
-    `title` VARCHAR(191) NOT NULL,
-    `content` JSON NULL,
-    `published` BOOLEAN NOT NULL DEFAULT false,
-    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `authorId` VARCHAR(191) NOT NULL,
+CREATE TABLE "verification_tokens" (
+    "identifier" VARCHAR(191) NOT NULL,
+    "token" VARCHAR(191) NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
 
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    UNIQUE ("token"),
+    UNIQUE ("identifier", "token")
+);
+
+-- CreateTable
+CREATE TABLE "posts" (
+    "id" VARCHAR(191) NOT NULL,
+    "title" VARCHAR(191) NOT NULL,
+    "content" JSON,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "authorId" VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY ("id")
+);
 
 -- AddForeignKey
-ALTER TABLE `accounts` ADD CONSTRAINT `accounts_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "accounts" ADD CONSTRAINT "accounts_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `sessions` ADD CONSTRAINT `sessions_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `posts` ADD CONSTRAINT `posts_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "posts" ADD CONSTRAINT "posts_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
